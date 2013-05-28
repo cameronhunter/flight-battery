4ca6aca2c92caeda51f9563ec99399244dca51cd
Cameron Hunter <chunter@twitter.com>
2013-05-28 15:11:21 -0700

    Add some tests

--- test/spec/battery.spec.js
+++ test/spec/battery.spec.js
@@ -1,11 +1,58 @@
 describeComponent('lib/battery', function () {
 
+  var lowBattery = {
+    level: 0.05,
+    charging: false
+  };
+
+  var charging = {
+    level: 0.75,
+    dischargingTime: Infinity,
+    charging: true
+  };
+
+  var discharging = {
+    level: 0.75,
+    charging: false
+  };
+
   beforeEach(function() {
-    setupComponent();
+    if (!window.navigator || !window.navigator.battery) {
+      window.navigator = {};
+      window.navigator.battery = charging;
+    }
+
+    setupComponent(document, {
+      lowLevel: 10
+    });
   });
 
-  it('should be defined', function() {
-    expect(this.component).toBeDefined();
+  it('should trigger "battery-status" on battery level changes', function() {
+    spyOn(navigator.battery).andReturn(discharging);
+    var eventSpy = spyOnEvent(document, 'battery-status');
+    $(document).trigger('levelchange');
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+  });
+
+  it('should trigger "battery-status-low" when battery drops below 10%', function() {
+    navigator.battery = lowBattery;
+    var eventSpy = spyOnEvent(document, 'battery-status-low');
+    $(document).trigger('levelchange');
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+  });
+
+  it('should trigger "battery-charging" when charging', function() {
+    navigator.battery = charging;
+    var eventSpy = spyOnEvent(document, 'battery-charging');
+    $(document).trigger('chargingchange');
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+  });
+
+  it('should trigger "battery-discharging" when discharging', function() {
+    navigator.battery = discharging;
+    var eventSpy = spyOnEvent(document, 'battery-discharging');
+    $(document).trigger('chargingchange');
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
   });
 
 });
